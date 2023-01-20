30ed2eb41004fd9c18b8f8ab4598795ffd075739
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-01-20 16:40:08 -0300

    Create functions for active check mark on contact selected

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ function KeepOnline() {
     const baseURL = "https://mock-api.driven.com.br/api/v6/uol/status"
     const user = { name: `${username}` };
     axios.post(baseURL, user);
-    console.log(username + " is Online");
+    //console.log(username + " is Online");
 }
 
 function success(resposta) {
-    console.log(resposta.status);
+    //console.log(resposta.status);
     message_alert.innerHTML = "";
-    //setInterval(KeepOnline(), 5000);
 
     (document.querySelector(".Login")).classList.add("Hidden");
+
+    GetParticipants();
+    GetMessages();
 
     setInterval(GetParticipants, 10000);
     setInterval(GetMessages, 3000);
@@ -22,14 +24,14 @@ function success(resposta) {
 }
 
 function fail(erro) {
-    console.log(erro.response.status)
+    //console.log(erro.response.status)
     message_alert.innerHTML = "Nome em uso, digite outro nome...";
 }
 
 function VerifyUser() {
     username = document.querySelector(".InputLogin").value;
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: username });
-    console.log(username + " entrou");
+    //console.log(username + " entrou");
     promise.then(success);
     promise.catch(fail);
 }
@@ -38,65 +40,53 @@ function VerifyUser() {
 
 // Inicio SIDEMENU
 
+const SideMenu = document.querySelector(".SideMenu");
+const Shadow = document.querySelector('.SideShadow');
+
 const ContactButton = document.querySelector('.ContactButton');
 ContactButton.addEventListener('click', (event) => {
-    const SideMenu = document.querySelector(".SideMenu");
-    SideMenu.classList.toggle('SideVisible');
-    SideMenu.classList.toggle('SideHidden');
-    const Shadow = document.querySelector('.SideShadow');
-    Shadow.classList.toggle('On');
-    Shadow.classList.toggle('SideHidden');
+    SideMenu.classList.add('SideVisible');
+    SideMenu.classList.remove('Hidden');
+    Shadow.classList.add('On');
+    Shadow.classList.remove('Hidden');
 })
 
-// Inicio SELECIONAR CONTATO
-let Contact = 'Todos';
-function SelectContact(){
-    let Contacts = document.querySelectorAll('.ListItem');
-    Contacts.forEach((select) => {
-        select.addEventListener('click', (event) => {
-            Contact = select.querySelector('.Selected').innerHTML;
-            const Check = document.querySelectorAll('.Check');
-            Check.forEach((checking) => {
-                checking.classList.toggle('CheckOn');
-                checking.classList.toggle('CheckOff');
-            })
-            console.log(`Você selecionou ${Contact}`);
-        })
-    })
-}
-// Fim SELECIONAR CONTATO
-
-let Visibility = 'message'
-// Inicio SELECIONAR VISIBILIDADE
-function SetVisibility(){
-    let Visible = document.querySelectorAll('.ListItem');
-    Visible.forEach((select) => {
-        select.addEventListener('click', (event) => {
-            if(select.querySelector('.SelectedVisibility').innerHTML === 'Publico'){
-                Visibility = 'message';
-                console.log(`Você selecionou enviar mensagens para Todos`);
-            }else if(select.querySelector('.SelectedVisibility').innerHTML === 'Reservadamente'){
-                Visibility = 'private_message';
-                console.log(`Você selecionou enviar mensagens privadas para ${Contact}`);
-            }
-        })
-    })
-}
-// Fim SELECIONAR VISIBILIDADE
-
 // Inicio SAIR MENU LATERAL
-const Shadow = document.querySelector('.SideShadow');
 Shadow.addEventListener('click', (event) => {
-    Shadow.classList.toggle('On');
-    Shadow.classList.toggle('SideHidden');
-    const SideMenu = document.querySelector(".SideMenu");
-    SideMenu.classList.toggle('SideVisible');
-    SideMenu.classList.toggle('SideHidden');
+    SideMenu.classList.remove('SideVisible');
+    SideMenu.classList.add('Hidden');
+    Shadow.classList.remove('On');
+    Shadow.classList.add('Hidden');
 })
 // Fim SAIR MENU LATERAL
 
-// Fim SIDEMENU
 
+let Contact = 'Todos';
+
+let Visibility = 'message'
+// Inicio SELECIONAR VISIBILIDADE
+const SetVisible = document.querySelectorAll('.ListItemVisibility');
+SetVisible.forEach((Visible) => {
+    Visible.addEventListener('click', (event) => {
+        let SetHidden = document.querySelectorAll('.Check', '.CheckOn');
+        SetHidden.forEach((UnSelect) => {
+            UnSelect.classList.add('CheckOff');
+            UnSelect.classList.remove('CheckOn');
+        })
+        Visible.querySelector('.Check').classList.add('CheckOn')
+        Visible.querySelector('.Check').classList.remove('CheckOff')
+        if(Visible.querySelector('.SelectedVisibility').innerHTML === 'Reservadamente'){
+            Visibility = 'private_message';
+        }else{
+            Visibility = 'message';
+        }
+        //console.log(Visibility)
+    })
+})
+// Fim SELECIONAR VISIBILIDADE
+
+
+// Fim SIDEMENU
 
 // Inicio MENSAGENS
 function GetMessages() {
@@ -154,19 +144,39 @@ function GetParticipants() {
 function LoadParticipants(Response) {
     const Participants = document.querySelector('.ContactsList');
     let ListP = Response.data;
-    Participants.innerHTML =   `<li class="ListItem" data-test="all" onclick="SelectContact()">
+    Participants.innerHTML =   `<li class="ListItem" data-test="all">
                                     <ion-icon class="IconList" name="people"></ion-icon> 
                                     <span class="Selected">Todos</span> 
+                                    <ion-icon name="checkmark-outline" class="Check CheckOff"></ion-icon>
                                 </li>`
 
     let Participant = ' ';
     for (let i = 0; i < ListP.length; i++) {
-        Participant = `<li class="ListItem" data-test="participant" onclick="SelectContact()">
+        Participant = `<li class="ListItem" data-test="participant">
                             <ion-icon name="person-circle"></ion-icon>
                             <span class="Selected">${ListP[i].name}</span>
+                            <ion-icon name="checkmark-outline" class="Check CheckOff"></ion-icon>
                         </li>`
 
         Participants.innerHTML += Participant;
+
+// Inicio SELECIONAR CONTATO
+        let SetContact = document.querySelectorAll('.ListItem');
+        SetContact.forEach((Select) => {
+            Select.addEventListener('click', (event) => {
+            let UnSelectContact = document.querySelectorAll('.Check', '.CheckOn');
+            UnSelectContact.forEach((UnSelect) => {
+                UnSelect.classList.add('CheckOff');
+                UnSelect.classList.remove('CheckOn');
+            })
+            Select.querySelector('.Check').classList.add('CheckOn');
+            Select.querySelector('.Check').classList.remove('CheckOff');
+            Contact = Select.querySelector('.Selected').innerHTML;
+            })
+        //console.log(Contact);
+        })
+// Fim SELECIONAR CONTATO
+
     }
 }
 
@@ -179,7 +189,7 @@ function SendMessage() {
             text: WriteMessage,
             type: Visibility
         });
-    console.log(WriteMessage);
+    //console.log(WriteMessage);
     const ClearText = document.querySelector('.TextMessage');
     ClearText.value = "";
 
